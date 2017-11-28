8bffa35f0dbcb8c2a81e2f28cbf8b2054af66503
jisub.yoon <yjisub11@gmail.com>
2017-11-28 09:55:46 +0900

    slack message test

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -5,7 +5,7 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <App name="Mark"/>,
+  <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
